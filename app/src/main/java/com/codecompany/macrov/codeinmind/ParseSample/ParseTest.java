59327156e9531b59d71baafec7cd484817806fbc
macrov.gao <macrov2.gao@sonymobile.com>
2014-10-16 14:57:30 +0800

    Add ParseSample.

---
+++ app/src/main/java/com/codecompany/macrov/codeinmind/ParseSample/ParseTest.java
@@ -0,0 +1,67 @@
+package com.codecompany.macrov.codeinmind.ParseSample;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.parse.GetCallback;
+import com.parse.GetDataCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.SaveCallback;
+
+/**
+ * Created by 28852028 on 10/16/2014.
+ */
+public class ParseTest {
+    Context context;
+    public ParseTest(Context context){
+        context = context;
+        //AppIP and ClientID
+        Parse.initialize(context,"ADAJl79Q7fQP5aq4WFQO1WFVZLgF001JjK0yvuSn", "AAEP8nHwnNc3XPB1XupvJASHdyXpBhyMQO34cWGJ");
+    }
+
+    public boolean createParseObject(){
+        ParseObject ob = new ParseObject("TestObject");
+        ob.put("Name","TestObject");
+        ob.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if( e == null ){
+                    Log.d("ParseTest", "Saved");
+                }
+            }
+        });
+    }
+
+    public void getCodeXml(){
+        ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Codes");
+        query.whereEqualTo("codeName","code");
+        query.getFirstInBackground(new GetCallback<ParseObject>() {
+            @Override
+            public void done(ParseObject parseObject, ParseException e) {
+                if( e == null ){
+                    ParseFile xmlFile = parseObject.getParseFile("code");
+                    if( xmlFile ){
+                        xmlFile.getDataInBackground(new GetDataCallback() {
+                            @Override
+                            public void done(byte[] bytes, ParseException e) {
+                                if( e == null ){
+                                    String xml = new String(bytes, null);
+                                    Log.d("ParseTest", xml);
+                                } else {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                    }
+                } else {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+}
